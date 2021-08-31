0eccb02b599cc327885e59d1e49ab156cab19b51
kirico-1027 <kuroneko0316@live.jp>
2021-08-31 10:25:43 +0000

    post-4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,9 @@
 class UsersController < ApplicationController
   
   def index
-    #@users = User.order(id: :desc)
   end
 
   def show
-    #@user = User.find(params[:id])
-    #@tasks = (@user.tasks.order(id: :desc))
-    #counts(@user)
   end
 
   def new
